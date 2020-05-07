6b93fa4d142f0155431f63ac562e9aa847f69c9c
kirian-cressey <scott.kirian-cressey@protonmail.com>
2020-05-07 19:32:03 -0400

    Added function to convert special charecters other than period to hyphens and added test strings.

--- unixify.py
+++ unixify.py
@@ -1,16 +1,37 @@
 def space_to_dash(string):
-    
+    """Takes a string and returns a new string with all whitespace converted
+        to hyphens."""
     # Return this string
     new_string = ''
 
     for i in string:
-        if i.isspace(): 
+        if i.isspace():
             new_string += '-'
         else:
             new_string += i
-    
+
     return new_string
 
-test = "I am a hippomouthamous."
+def special_to_dash(string):
+    """Takes a string and returns a new string with non-period special
+        charecters converted to hyphens."""
+    # note: this is seperated from space_to_dash() because I would like to
+    # add optional command line arguments in the future to choose not to 
+    # convert special charecters. There are situations where this could be
+    # useful.
 
-print(space_to_dash(test))
+    new_string = ''
+
+    for i in string:
+        # If i is not a number, letter, or period, convert i to hyphen
+        if not i.isalnum() and i != '.':
+            new_string += '-'
+        else:
+            new_string += i
+
+    return new_string
+
+# These strings are being used for testing/debugging during development
+symbols = "~`!@#$%^&*()_-+={[}]|:;'<,>.?/"
+test = "I am a h!ppomoutham*u$."
+unenthused_world = "Hello, World."
