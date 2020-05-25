b4eb165f81708bfe5681f7799940d9f174ddfd6e
kirian-cressey <scott.kirian-cressey@protonmail.com>
2020-05-25 12:47:13 -0400

    Added CLI and warning message.
    special_to_dash() will now not allow '-' as first character in a file name.
    
    Added Horrible No Good Very Bad test directories for testing filename changes.

---
+++ test_dir/READ_ME.txt
@@ -0,0 +1,8 @@
+This is a set of heinously named files and directories made for the 
+sole purpose of renaming them while testing a program. 
+
+There is nothing whatsoever of any value here.
+
+Beware, ye who plumb these depths, 
+for the night is dark and full of
+terrors.
--- unixify.py
+++ unixify.py
@@ -1,12 +1,14 @@
-def space_to_dash(string):
+import sys, os
+
+def space_to_underscore(string):
     """Takes a string and returns a new string with all whitespace converted
-        to hyphens."""
+        to underscores."""
     # Return this string
     new_string = ''
 
     for i in string:
         if i.isspace():
-            new_string += '-'
+            new_string += '_'
         else:
             new_string += i
 
@@ -17,21 +19,48 @@ def special_to_dash(string):
         charecters converted to hyphens."""
     # note: this is seperated from space_to_dash() because I would like to
     # add optional command line arguments in the future to choose not to 
-    # convert special charecters. There are situations where this could be
-    # useful.
+    # convert special charecters. There may be situations where this could be
+    # useful. Not sure. Will look into this and refactor if I see no use.
 
     new_string = ''
 
     for i in string:
-        # If i is not a number, letter, or period, convert i to hyphen
-        if not i.isalnum() and i != '.':
+        # We will permit only numbers, letters, and these characters:
+        permited_chars = ['.', '-', '_']
+
+        if not i.isalnum() and not i in permited_chars:
             new_string += '-'
         else:
             new_string += i
 
+    # File names should not start with a hyphen
+    if new_string[0] == '-':
+        new_string = '_' + new_string[1:]
+
     return new_string
 
-# These strings are being used for testing/debugging during development
-symbols = "~`!@#$%^&*()_-+={[}]|:;'<,>.?/"
-test = "I am a h!ppomoutham*u$."
-unenthused_world = "Hello, World."
+
+def walk(targ_dir):
+    files = []
+    dirs = []
+
+    for item in os.listdir(targ_dir):
+        print(item)
+
+if __name__ == '__main__':
+
+    directory = sys.argv[1]
+    warning = """You are about to rename all files and directories in {}
+and rename all files and directories contained therein.
+This operation cannot be undone.
+Please enter 'yes' if you have checked the directory path
+and are absolutely sure.""".format(directory)
+
+    print(warning)
+    response = input("\nContinue?\n>>> ").lower()
+
+    if response == 'y' or response == 'yes':
+        print(special_to_dash('-Pi$$a party time'))
+        walk(directory)
+    else:
+        sys.exit()
